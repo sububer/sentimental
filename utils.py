6d37f6632522731cefcdc6c47d131c7ace570198
shivangiuw <shivangigupta.india@gmail.com>
2022-03-23 16:52:50 -0700

    changes added

--- utils.py
+++ utils.py
@@ -19,34 +19,34 @@ stop_words= set(stopwords.words('english'))
 
 def clean_df(df):
     
-df['created_at'] = pd.to_datetime(df['created_at']
+    df['created_at'] = pd.to_datetime(df['created_at'])
 
-pub_metz = pd.Series(df["public_metrics"])
-pm_df = pd.DataFrame(columns=['retweet_count', 'reply_count', 'like_count', 'quote_count'])
+    pub_metz = pd.Series(df["public_metrics"])
+    pm_df = pd.DataFrame(columns=['retweet_count', 'reply_count', 'like_count', 'quote_count'])
 
-k = []
-for d in pub_metz:
-    k.append(d)
+    k = []
+    for d in pub_metz:
+        k.append(d)
     
-pub_metz_clean = pd.DataFrame(k)
+    pub_metz_clean = pd.DataFrame(k)
                                         
-df= pd.concat([df,pub_metz_clean], axis=1)                                
-df= df.drop(columns= ["public_metrics","entities","id"])
+    df= pd.concat([df,pub_metz_clean], axis=1)                                
+    df= df.drop(columns= ["public_metrics","entities","id"])
                                 
-text_df= df["text"]
-df= df.drop(columns= "text")
+    text_df= df["text"]
+    #df= df.drop(columns= "text")
                                 
-return df
+    return df
 
                                   
 ##  Preprocessing text in text column
                                   
 def preprocess_text(df):
+    texts= df["text"]
     lemma_words= []
-    for text in df.iteritems():
-       
+    for text in texts:
     # convert text to lower case
-        text= text[1].lower()
+        text= text.lower()
     
     # remove urls if any
         text= re.sub(r'(http\s|ftp\s|https\s|www)(://|\.)[A-Za-z0-9-_\.]*(\.)[a-z]', " " , text, flags= re.MULTILINE)
@@ -73,6 +73,7 @@ def preprocess_text(df):
 ## Converting preprocessed text to vectors
                                   
 def get_feature_vector(lemma_words):
+    print(lemma_words)
     vector= TfidfVectorizer(sublinear_tf= True)
     X = vector.fit_transform(lemma_words)
     print(X.shape)
